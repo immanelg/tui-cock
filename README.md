4b3bfc146c999a575fb50a7f46ab7bd302ae3e57
immanelg <119798691+immanelg@users.noreply.github.com>
2025-01-31 23:31:06 +0300

    add cock sizes

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Terminal cock
 
 ## Install
-`cargo install --locked --git https://github.com/immanelg/tui-cock`
+
+```
+cargo install --locked --git https://github.com/immanelg/tui-cock
+```
+
+```
+cargo install --locked tui-cock
+```
+
 ## Run
-`tui-cock`
+```
+tui-cock 20
+```
+
+The argument is the size of the cock. It is probably between 10 and 69, should be easy to try different values to see what looks good for your terminal.
+
+## Zellij
+The cock looks nice in a zellij layout, e.g.
+
+layout {
+    pane command="btm" size="65%"
+    pane split_direction="vertical" {
+        pane {
+            command "gping"
+            args "192.168.50.1" "8.8.8.8" "1.1.1.1" "-n" "1" "-b" "300"
+        }
+        pane size="20%" {
+            command "tui-cock"
+            args "20"
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::io::Write;
+
 fn main() {
-    println!("
+    let mut stdout = std::io::stdout();
+    let first_part = &"\
 ⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢀⣾⡿⠋⠀⠿⠇⠉⠻⣿⣄⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⢠⣿⠏⠀⠀⠀⠀⠀⠀⠀⠙⣿⣆⠀⠀⠀⠀⠀
@@ -7,12 +10,8 @@ fn main() {
 ⠀⠀⠀⠀⢸⣿⡄⠀⠀⠀⢀⣤⣀⠀⠀⠀⠀⣿⡿⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠻⣿⣶⣶⣾⡿⠟⢿⣷⣶⣶⣿⡟⠁⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⣿⡏⠉⠁⠀⠀⠀⠀⠉⠉⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
+";
+    let third_part = &"\
 ⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⣸⣿⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⣿⡇⢀⣴⣿⠇⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀
 ⠀⠀⠀⢀⣠⣴⣿⣷⣿⠟⠁⠀⠀⠀⠀⠀⣿⣧⣄⡀⠀⠀⠀
@@ -23,6 +22,21 @@ fn main() {
 ⢹⣿⡄⠀⠀⠀⠀⠀⠀⢿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡿
 ⠀⠻⣿⣦⣀⠀⠀⠀⠀⠈⣿⣷⣄⡀⠀⠀⠀⠀⣀⣤⣾⡟⠁
 ⠀⠀⠈⠛⠿⣿⣷⣶⣾⡿⠿⠛⠻⢿⣿⣶⣾⣿⠿⠛⠉⠀⠀
-");
-}
+";
 
+    stdout.write_all(first_part.as_bytes()).unwrap();
+
+    const DEFAULT_SIZE: usize = 0;
+    let size = std::env::args()
+        .nth(1)
+        .map(|n| n.parse::<usize>().unwrap_or(DEFAULT_SIZE))
+        .unwrap_or(DEFAULT_SIZE);
+    let repeated_thing = &"⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇
+";
+    for _ in 0..size {
+        stdout.write_all(repeated_thing.as_bytes()).unwrap();
+    }
+
+    stdout.write_all(third_part.as_bytes()).unwrap();
+    stdout.flush().unwrap();
+}
